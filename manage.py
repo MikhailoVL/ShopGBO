ae893c9b095ac2e72d8712da70c6fb5a83e98f91
MikhailoVL <mlyutyyv@gmail.com>
2020-05-24 17:29:47 +0300

    rename

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gbo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysitegbo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- mysitegbo/asgi.py
+++ mysitegbo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for gbo project.
+ASGI config for mysitegbo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gbo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysitegbo.settings')
 
 application = get_asgi_application()
